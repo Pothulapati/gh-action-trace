43d4b94fd72ef989a33eb616cda55fd39dc19886
Tarun Pothulapati <tarunpothulapati@outlook.com>
2021-12-13 23:21:06 +0530

    new `v0.2.0` version
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use opentelemetry::KeyValue;
 /// intended to be run as a standalone binary.
 #[derive(Parser)]
 #[clap(
-    version = "0.1.0",
+    version = "0.2.0",
     author = "Tarun Pothulapati <tarunpothulapati@outlook.com>"
 )]
 struct Opts {
